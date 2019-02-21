6e89f4418720135a0b958ed269aef50c14eed80e
Yujiro Ito <yujiro.bear@gmail.com>
2019-02-21 17:01:46 +0900

    球と球のあたり判定を実装

---
+++ Assets/Scripts/SphereHitTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereHitTest : MonoBehaviour {
+    [SerializeField] Transform _sphereOne;
+    [SerializeField] Transform _sphereTwo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Mathf.Sqrt( Mathf.Pow(_sphereTwo.position.x - _sphereOne.position.x, 2) +
+                                     Mathf.Pow(_sphereTwo.position.y - _sphereOne.position.y, 2) +
+                                     Mathf.Pow(_sphereTwo.position.z - _sphereOne.position.z, 2));
+        float rad = (_sphereOne.localScale.x + _sphereTwo.localScale.x) / 2;
+
+        if(distance < rad)
+        {
+            Debug.Log("hit");
+        } else
+        {
+            Debug.Log("not hit");
+        }
+	}
+}
